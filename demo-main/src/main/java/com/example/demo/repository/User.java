cfa9217a68ba1a5aacffbeebed968e9e6863e0fd
urovik <your_email@example.com>
2025-03-10 20:56:56 +0500

    commit first

---
+++ demo-main/src/main/java/com/example/demo/repository/User.java
@@ -0,0 +1,86 @@
+package com.example.demo.repository;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "test")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    private String email;
+    @Column(name = "birth")
+    private LocalDate birth;
+    private Integer age;
+
+    public User(String name, String email, LocalDate birth, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.birth = birth;
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birth=" + birth +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ demo-main/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,65 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User create(User user) {
+        Optional<User> optionalUser = userRepository.findByEmail(user.getEmail());
+        if (optionalUser.isPresent()) {
+            throw new IllegalStateException("юзер с таким имейлом уже существует");
+        }
+        user.setAge(Period.between(user.getBirth(), LocalDate.now()).getYears());
+        return userRepository.save(user);
+    }
+    
+    public void delete(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty()) {
+            throw new IllegalStateException("юзера с id: " + id + " не существует");
+        }
+        userRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void update(Long id, String email, String name) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty()) {
+            throw new IllegalStateException("юзера с id: " + id + " не существует");
+        }
+        User user = optionalUser.get();
+
+        if (email != null && !email.equals(user.getEmail())) {
+            Optional<User> foundByEmail = userRepository.findByEmail(email);
+            if (foundByEmail.isPresent()) {
+                throw new IllegalStateException("юзер с таким имейлом уже существует");
+            }
+            user.setEmail(email);
+        }
+
+        if (name != null && !name.equals(user.getName())) {
+            user.setName(name);
+        }
+
+//        userRepository.save(user);
+    }
+}
